064bcb02f33c79f36620d0952f038dd1548727b5
Riley Moore <rilemoore@gmail.com>
2019-11-06 16:58:54 +0000

    inserted bears

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Mr. Chocolate", 12, "male", "black", "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Rowdy", 12, "male", "black", "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Tabitha", 12, "male", "black", "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Sergeant Brown", 12, "male", "black", "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Melissa", 12, "male", "black", "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Grinch", 12, "male", "black", "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Wendy", 12, "male", "black", "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES (NULL, 12, "male", "black", "happy", 1);
